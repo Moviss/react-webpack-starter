3d1e9686a4df3e4d82a1c02ffabf6d59840bf513
Marcin Lubowicz <33049437+Moviss@users.noreply.github.com>
2020-01-08 10:12:33 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,78 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
-const settings = {
-  distPath: path.join(__dirname, "dist"),
-  srcPath: path.join(__dirname, "src")
+const isDevMode = true;
+
+var config = {
+  devtool: "source-map",
+  resolve: {
+    extensions: [".js", ".jsx"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: true
+            }
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              plugins: [require("autoprefixer")()],
+              sourceMap: true
+            }
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(ttf|eot|woff|woff2)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "fonts/[name].[ext]"
+          }
+        }
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg|ico)$/i,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              outputPath: "assets/"
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin({
+      verbose: true
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    })
+  ]
 };
 
 function srcPathExtend(subpath) {
@@ -12,76 +81,5 @@ function srcPathExtend(subpath) {
 }
 
 module.exports = (env, options) => {
-  const isDevMode = options.mode === "development";
-
-  return {
-    devtool: isDevMode ? "source-map" : false,
-    resolve: {
-      extensions: [".js", ".jsx"]
-    },
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /mode_modules/,
-          use: {
-            loader: "babel-loader"
-          }
-        },
-        {
-          test: /\.scss$/,
-          use: [
-            "style-loader",
-            {
-              loader: "css-loader",
-              options: {
-                sourceMap: isDevMode
-              }
-            },
-            {
-              loader: "postcss-loader",
-              options: {
-                plugins: [require("autoprefixer")()],
-                sourceMap: isDevMode
-              }
-            },
-            {
-              loader: "sass-loader",
-              options: {
-                sourceMap: isDevMode
-              }
-            }
-          ]
-        },
-        {
-          test: /\.(ttf|eot|woff|woff2)$/,
-          use: {
-            loader: "file-loader",
-            options: {
-              name: "fonts/[name].[ext]"
-            }
-          }
-        },
-        {
-          test: /\.(jpe?g|png|gif|svg|ico)$/i,
-          use: [
-            {
-              loader: "file-loader",
-              options: {
-                outputPath: "assets/"
-              }
-            }
-          ]
-        }
-      ]
-    },
-    plugins: [
-      new CleanWebpackPlugin({
-        verbose: true
-      }),
-      new HtmlWebpackPlugin({
-        template: srcPathExtend("index.html")
-      })
-    ]
-  };
+  return config;
 };
